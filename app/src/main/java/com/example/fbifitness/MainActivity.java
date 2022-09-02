99083df759d4a8e8ba8cd21d77869049f69805c8
Peter Smith <plasmatechstudios@gmail.com>
2022-09-02 14:13:59 +1000

    Updated fragment manager

--- app/src/main/java/com/example/fbifitness/MainActivity.java
+++ app/src/main/java/com/example/fbifitness/MainActivity.java
@@ -12,36 +12,49 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    public Fragment communityFragment = new CommunityFragment();
+    public Fragment reportsFragment = new ReportsFragment();
+    public Fragment newWorkoutFragment = new NewWorkoutFragment();
+    public Fragment badgeFragment = new BadgeFragment();
+    public Fragment profileFragment = new ProfileFragment();
+
     ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+//        Fragment communityFragment = new CommunityFragment();
+//        Fragment reportsFragment = new ReportsFragment();
+//        Fragment newWorkoutFragment = new NewWorkoutFragment();
+//        Fragment badgeFragment = new BadgeFragment();
+//        Fragment profileFragment = new ProfileFragment();
+
         BottomNavigationView bottomNavigationView;
         bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
         bottomNavigationView.setSelectedItemId(R.id.profile);
-        replaceFragment(new ProfileFragment()); // Replace this with login screen later
+        replaceFragment(profileFragment); // Replace this with login screen later
 
         binding.bottomNavigationView.setOnItemSelectedListener(item -> {
 
             switch(item.getItemId()) {
                 case R.id.community:
-                    replaceFragment(new CommunityFragment());
+                    replaceFragment(communityFragment);
                     break;
                 case R.id.reports:
-                    replaceFragment(new ReportsFragment());
+                    replaceFragment(reportsFragment);
                     break;
                 case R.id.newWorkout:
-                    replaceFragment(new NewWorkoutFragment());
+                    replaceFragment(newWorkoutFragment);
                     break;
                 case R.id.badges:
-                    replaceFragment(new BadgeFragment());
+                    replaceFragment(badgeFragment);
                     break;
                 case R.id.profile:
-                    replaceFragment(new ProfileFragment());
+                    replaceFragment(profileFragment);
                     break;
             }
             return true;
